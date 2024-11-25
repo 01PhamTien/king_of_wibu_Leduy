092f90f98e17957f38208ef6afe15f582b436487
01PhamTien <151815547+01PhamTien@users.noreply.github.com>
2024-11-25 14:25:57 +0700

    Lỏ Định Code sai tùm lum

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import './assets/css/style.css';  // Tệp CSS chung chứa tất cả các import
+
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';  // Import CSS của Bootstrap
