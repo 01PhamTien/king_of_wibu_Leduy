8caf0898ee932ee97ca569da7d6c37b17b1b4a74
01PhamTien <151815547+01PhamTien@users.noreply.github.com>
2024-12-03 12:17:13 +0700

    update cho duy lỏ

--- frontend/src/data/data.js
+++ frontend/src/data/data.js
@@ -71,6 +71,79 @@ const rooms = [
     reviews: 180, 
     image: '/images/008.jpg' 
   },
+  { 
+    id: 9, 
+    name: 'New Star Inn Boutique Hotel', 
+    address: 'TP. Hồ Chí Minh, Việt Nam', 
+    price: '1,000,000 VND', 
+    rating: 4.5, 
+    reviews: 100, 
+    image: '/images/001.jpg' 
+  },
+  { 
+    id: 10, 
+    name: 'Skylark Boutique Hotel', 
+    address: 'Hà Nội, Việt Nam', 
+    price: '1,200,000 VND', 
+    rating: 3, 
+    reviews: 45, 
+    image: '/images/002.jpg' 
+  },
+  { 
+    id: 11, 
+    name: 'Flamingo Hotel', 
+    address: 'Đà Nẵng, VIệt Nam', 
+    price: '900,000 VND', 
+    rating: 4, 
+    reviews: 200, 
+    image: '/images/003.jpg' 
+  },
+  { 
+    id: 12, 
+    name: 'Sài Gòn Hotel', 
+    address: 'Sài Gòn, Việt Nam', 
+    price: '1,500,000 VND', 
+    rating: 5, 
+    reviews: 350, 
+    image: '/images/004.jpg' 
+  },
+  { 
+    id: 13, 
+    name: 'Galaxy Boutique Sai Gon', 
+    address: 'TP. Hồ Chí Minh, Việt Nam', 
+    price: '1,800,000 VND', 
+    rating: 4.5, 
+    reviews: 30, 
+    image: '/images/005.jpg' 
+  },
+  { 
+    id: 14, 
+    name: 'Ipeace Hotel', 
+    address: 'TP. Hồ Chí Minh, Việt Nam', 
+    price: '1,950,000 VND', 
+    rating: 3.5, 
+    reviews: 150, 
+    image: '/images/006.jpg' 
+  },
+  { 
+    id: 15, 
+    name: 'The Reverie Saigon', 
+    address: 'TP. Hồ Chí Minh, Việt Nam', 
+    price: '3,300,000 VND', 
+    rating: 4.8, 
+    reviews: 220, 
+    image: '/images/007.jpg' 
+  },
+  { 
+    id: 16, 
+    name: 'Love Hotel',
+address: 'Hue, Việt Nam', 
+    price: '2,000,000 VND', 
+    rating: 4.2, 
+    reviews: 180, 
+    image: '/images/008.jpg' 
+  },
+  
 ];
 
 export default rooms;
